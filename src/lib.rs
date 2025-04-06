b8868829a96be7def764c95042a5f04e798ff434
TimTheBig <132001783+TimTheBig@users.noreply.github.com>
2025-04-06 16:44:50 -0400

    Document features

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,27 @@
 //!
 //! ![Example CSG output][Example CSG output]
 #![cfg_attr(doc, doc = doc_image_embed::embed_image!("Example CSG output", "docs/csg.png"))]
+//!
+//! # Features
+//! #### Default
+//! - **f64**: use f64 as Real
+//! - [**stl-io**](https://en.wikipedia.org/wiki/STL_(file_format)): `.stl` import/export
+//! - [**dxf-io**](https://en.wikipedia.org/wiki/AutoCAD_DXF): `.dxf` import/export
+//! - **chull-io**: convex hull and minkowski sum
+//! - **metaballs**: enables a `CSG` implementation of [metaballs](https://en.wikipedia.org/wiki/Metaballs)
+//! - **hashmap**: enables use of hashbrown for slice, related helper functions, and `is_manifold`
+//! - **sdf**: signed distance fields ([sdf](https://en.wikipedia.org/wiki/Signed_distance_function)) using [fast-surface-nets](https://crates.io/crates/fast-surface-nets)
+//! - **offset**: use `geo-buf` for offset operations
+//! - **delaunay**: use `geo`s `spade` feature for triangulation
+//!
+//! #### Optional
+//! - **f32**: use f32 as Real, this conflicts with f64
+//! - **parallel**: use rayon for multithreading
+//! - **svg-io**: create `CSG`s from and convert `CSG`s to SVG's
+//! - **truetype-text**: create `CSG`s using TrueType fonts `.ttf`
+//! - **hershey-text**: create `CSG`s using Hershey fonts (`.jhf`)
+//! - **image-io**: make 2d `CSG`s from images
+//! - **earcut**: use `geo`s `earcutr` feature for triangulation
 
 #![forbid(unsafe_code)]
 #![deny(unused)]
