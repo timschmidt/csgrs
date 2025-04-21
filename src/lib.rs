5d0df5f1f4c484614f4a0a46be6bd26dc8742520
TimTheBig <132001783+TimTheBig@users.noreply.github.com>
2025-04-20 20:19:58 -0400

    Add check that Real is defined

--- src/lib.rs
+++ src/lib.rs
@@ -39,6 +39,9 @@ pub mod shapes3d;
 pub mod extrudes;
 pub mod io;
 
+#[cfg(any(all(feature = "f64", feature = "f32"), not(any(feature = "f64", feature = "f32"))))]
+compile_error!("Either 'f64' or 'f32' feature must be specified, but not both");
+
 #[cfg(any(all(feature = "delaunay", feature = "earcut"), not(any(feature = "delaunay", feature = "earcut"))))]
 compile_error!("Either 'delaunay' or 'earcut' feature must be specified, but not both");
 
