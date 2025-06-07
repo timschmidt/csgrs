b2f68c680aa213c18cfe18dd8f1c97eca43f2e78
TimTheBig <132001783+TimTheBig@users.noreply.github.com>
2025-06-07 15:20:12 -0400

    Fix bevy mesh

--- examples/bevy_mesh.rs
+++ examples/bevy_mesh.rs
@@ -1,21 +1,17 @@
 //! This example demos converting a `CSG` to a bevy mesh
 
 use csgrs::CSG;
-use std::{fs, path::Path};
 
 const PATH: &str = "stl/bevymesh";
 
 fn main() {
     #[cfg(not(feature = "bevymesh"))]
-    compile_error!("This example requires the `bevymesh` feature to be enabled");
+    panic!("This example requires the `bevymesh` feature to be enabled");
 
     #[cfg(feature = "bevymesh")]
-    println!("{:#?}", bezier_3d.to_bevy_mesh());
-}
+    {
+        let cube = CSG::<()>::cube(2.0, 2.0, 2.0, None);
 
-fn write_example(shape: &CSG, name: &str) {
-    let _ = fs::write(
-        Path::new(PATH).join(name).with_extension("stl"),
-        shape.to_stl_binary(name).unwrap(),
-    );
+        println!("{:#?}", cube.to_bevy_mesh());
+    }
 }
