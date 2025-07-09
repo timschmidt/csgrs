8928f125f91cd57458581345eb76adb3b62ddae7
Timothy Schmidt <timschmidt@gmail.com>
2025-07-09 11:28:29 -0400

    add test for triangles and early out to Polygon::triangulate

--- src/mesh/polygon.rs
+++ src/mesh/polygon.rs
@@ -129,6 +129,15 @@ impl<S: Clone + Send + Sync> Polygon<S> {
         if self.vertices.len() < 3 {
             return Vec::new();
         }
+        
+        // A polygon that is already a triangle: no need to call earcut/spade.
+        // Returning it directly avoids robustness problems with very thin
+        // triangles and makes the fast-path cheaper.
+        if self.vertices.len() == 3 {
+            return vec![[self.vertices[0].clone(),
+                         self.vertices[1].clone(),
+                         self.vertices[2].clone()]];
+        }
 
         let normal_3d = self.plane.normal().normalize();
         let (u, v) = build_orthonormal_basis(normal_3d);
