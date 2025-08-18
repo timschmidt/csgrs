e2c5c14c2b7d46b97839273d0ce36b58ee2d7570
Mark van der Net <mark@oscity.nl>
2025-08-18 19:10:24 +0200

    Basic setup for a wasm TS/JS module

---
+++ csgrs-wasm/starter-templates/nuxt4/app/app.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    csgrs-wasm
+  </div>
+</template>
+
+<script setup>
+
+const loadCsgrs = async () => 
+{
+  if (process.client) 
+  {
+    const csgrs = (await import('../../../bin/csgrs_wasm.js'));
+
+    console.log(csgrs)
+
+    const vertex = new csgrs.VertexJs(1.0, 2.0, 3.0)
+    console.log(vertex)
+    console.log('Vertex array:', vertex.to_array())
+    
+    return csgrs;
+  }
+}
+
+await loadCsgrs();
+
+</script>
